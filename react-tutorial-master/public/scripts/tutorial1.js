f156fefcd3e16051aa81f1f21161a55b76da6ae0
Sam Szuflita <sszuflit@caltech.edu>
2015-09-13 14:03:20 -0400

    Loading comments from server works.

--- react-tutorial-master/public/scripts/tutorial1.js
+++ react-tutorial-master/public/scripts/tutorial1.js
@@ -17,7 +17,7 @@ var CommentList = React.createClass({
 			<div className="commentList">
 				{commentNodes}
 			</div>
-		);
+			);
 	}
 });
 
@@ -32,11 +32,35 @@ var CommentForm = React.createClass({
 });
 
 var CommentBox = React.createClass({
+
+	getInitialState: function() {
+		return {data: []};
+	},
+
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
+
 	render: function() {
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.props.data} />
+				<CommentList data={this.state.data} />
 				<CommentForm />
 			</div>
 		);
@@ -58,6 +82,6 @@ var Comment = React.createClass({
 });
 
 React.render(
-	<CommentBox data={data} />,
+	<CommentBox url="comments.json" pollInterval={2000} />,
 	document.getElementById('content')
 );
